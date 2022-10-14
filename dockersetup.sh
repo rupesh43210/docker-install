015bd294d98c97577c9de3d0a6ba78c02fdbb302
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-10-14 12:48:49 +0530

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -26,3 +26,15 @@ sudo apt-get install -y \
  sudo apt autoremove -y
  
  echo "Docker Install Completed" 
+ 
+ docker volume create portainer_data
+ 
+ docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+    
+ clear
+ 
+ echo "Please visit https://localhost:9443"
