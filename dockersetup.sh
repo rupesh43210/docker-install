54c8f3d621a49523e19de46f62b59ed31d172369
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-10-14 12:16:13 +0530

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -16,4 +16,4 @@ sudo apt-get install \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
  sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
