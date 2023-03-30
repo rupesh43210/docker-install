e58ec05e46c6f0b786866e1d39e980fad079d995
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2023-03-30 13:37:15 +0530

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -16,10 +16,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
-  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get remove docker docker-engine docker.io containerd runc -y
   
  sudo apt-get update 
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
  
  cd ../
  rm -r docker-install/
