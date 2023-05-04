694f729bb0b61c86fc583195e1c3531654baf432
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2023-05-04 13:58:47 +0530

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,54 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
+# Update package lists and install dependencies
 sudo apt update
-
 sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-    
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+# Add Docker GPG key and repository
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
- 
- echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-  sudo apt-get remove docker docker-engine docker.io containerd runc -y
-  
- sudo apt-get update 
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
- 
- cd ../
- rm -r docker-install/
- 
- clear 
- 
- sudo apt autoremove -y
- 
- echo "Docker Install Completed" 
- 
- sudo groupadd docker
- sudo usermod -aG docker $USER
- newgrp docker
- 
- 
- 
- 
- 
- docker volume create portainer_data
- 
- docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce:latest
-    
- cd ../
- rm -r docker-install/
-    
- clear
- 
- echo "Please visit https://localhost:9443"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Remove old versions of Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+# Install Docker and Docker Compose
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+# Add current user to the Docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+echo "Please log out and log back in to apply group changes."
+
+# Create Portainer data volume and start Portainer container
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# Clean up
+sudo apt autoremove -y
+echo "Docker Install Completed"
